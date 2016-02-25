6a6af1aa40318aa07ed6ac5d3ea1ad15c7140554
awebre <austin.webre@gmail.com>
2016-02-25 15:44:06 -0600

    Added PlanModel, fixed error on configuration.cs

---
+++ SlickScheduler/SlickScheduler/Models/PlanModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SlickScheduler.Models
+{
+    public class PlanModel : DbContext
+    {
+        public PlanModel()
+            : base("name=ProjectDB")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<PlanModel, SlickScheduler.Migrations.Configuration>("ProjectDB"));
+        }
+
+        public virtual DbSet<Plan> CMPS_IT_2013 { get; set; }
+    }
+
+    public class Plan
+    {
+        public int Id { get; set; }
+        public int Semester { get; set; }
+    }
+}
