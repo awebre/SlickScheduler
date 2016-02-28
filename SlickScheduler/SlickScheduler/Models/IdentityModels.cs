11b9c045b4907b3b381ddf25efad832612f7dc1d
awebre <austin.webre@gmail.com>
2016-02-28 11:59:11 -0600

    Connected User Registration to projects database.

--- SlickScheduler/SlickScheduler/Models/IdentityModels.cs
+++ SlickScheduler/SlickScheduler/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SlickScheduler.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ProjectDB", throwIfV1Schema: false)
         {
         }
 
