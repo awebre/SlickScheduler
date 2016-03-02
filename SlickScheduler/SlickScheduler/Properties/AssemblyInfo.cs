b8eb1f4157b457aa41e6919a2f8ea3a4afed7902
awebre <austin.webre@gmail.com>
2016-03-02 13:39:11 -0600

    Finally cleaned up the project.

--- SlickScheduler/SlickScheduler/Properties/AssemblyInfo.cs
+++ SlickScheduler/SlickScheduler/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d22dba9-6a13-4fd1-9d23-e5a9bc1ad128")]
+[assembly: Guid("849cff80-c6f3-400f-a514-d2c403376309")]
 
 // Version information for an assembly consists of the following four values:
 //
