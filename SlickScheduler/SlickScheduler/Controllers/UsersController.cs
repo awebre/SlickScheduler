308179bd21c005ca1f3111e048dc0f7571847364
awebre <austin.webre@gmail.com>
2016-03-03 16:55:06 -0600

    Included ApplicationInsights.cs

--- SlickScheduler/SlickScheduler/Controllers/UsersController.cs
+++ SlickScheduler/SlickScheduler/Controllers/UsersController.cs
@@ -68,18 +68,6 @@ namespace SlickScheduler.Controllers
         // POST: Users/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "UserId,FirstName,LastName,Email,Password,UserName")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(user);
-        }
 
         // GET: Users/Delete/5
         public ActionResult Delete(int? id)
