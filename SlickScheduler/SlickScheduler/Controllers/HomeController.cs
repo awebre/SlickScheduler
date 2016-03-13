126265a7fe735a31dc0ca5e2e936048f73fc8581
awebre <austin.webre@gmail.com>
2016-03-13 17:35:36 -0500

    Strated creating scheduler view for students. Added Computer Science and English courses to the view.

--- SlickScheduler/SlickScheduler/Controllers/HomeController.cs
+++ SlickScheduler/SlickScheduler/Controllers/HomeController.cs
@@ -32,9 +32,56 @@ namespace SlickScheduler.Controllers
 
         public ActionResult Scheduler()
         {
+            var currentUser = db.Users.ToList().Single(u => u.Email == HttpContext.User.Identity.Name);
+            List<Course> Math = new List<Course>();
+            List<Course> English = new List<Course>();
+            List<Course> Science = new List<Course>();
+            List<Course> History = new List<Course>();
+            List<Course> Elective = new List<Course>();
+            List<Course> CMPS = new List<Course>();
+            var semesters = currentUser.Student.Plan.Semesters;
+            var courses = new List<Course>();
+            if (currentUser != null && currentUser.Student != null)
+            {
+                foreach (var s in semesters)
+                {
+                    foreach (var c in s.Courses)
+                    {
+                        courses.Add(c);
+                        switch (c.Subject)
+                        {
+                            case "MATH":
+                                Math.Add(c);
+                                break;
+                            case "ENGL":
+                                English.Add(c);
+                                break;
+                            case "SCI":
+                                Science.Add(c);
+                                break;
+                            case "HIST":
+                                History.Add(c);
+                                break;
+                            case "CMPS":
+                                CMPS.Add(c);
+                                break;
+                            default:
+                                Elective.Add(c);
+                                break;
+                        }
+                    }
+                }
+                ViewBag.AllCourses = courses;
+                ViewBag.Math = Math;
+                ViewBag.English = English;
+                ViewBag.Science = Science;
+                ViewBag.History = History;
+                ViewBag.CMPS = CMPS;
+                ViewBag.Electives = Elective;
+                ViewBag.CurrentUser = currentUser;
+            }
 
             return View();
         }
-       
     }
 }
