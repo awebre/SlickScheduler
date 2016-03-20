cb94b16cfb7481db03b15f9401a8910c281ac29d
awebre <austin.webre@gmail.com>
2016-03-20 14:00:25 -0500

    Began work on admin controller and views as well as cleaned up old controllers.

---
+++ SlickScheduler/SlickScheduler/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using SlickScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlickScheduler.Controllers
+{
+    public class AdminController : Controller
+    {
+        private DataModelContext db = new DataModelContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.Users = db.Users.ToList();
+            return View();
+        }
+    }
+}
