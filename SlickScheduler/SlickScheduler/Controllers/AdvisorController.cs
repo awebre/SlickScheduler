42082aa2412e2b57c3b6374c3445376ea7311cec
rvanderbrook <rvanderbrook@gmail.com>
2016-03-24 14:58:46 -0700

    added advisor controller - ryan and chris

---
+++ SlickScheduler/SlickScheduler/Controllers/AdvisorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlickScheduler.Controllers
+{
+    public class AdvisorController : Controller
+    {
+        // GET: Advisor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
