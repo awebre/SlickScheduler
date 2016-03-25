12fdff5b3930738aa979e0a607efc84d236c1292
awebre <austin.webre@gmail.com>
2016-03-24 23:27:06 -0500

    Added sorting by name and wnumber, as well as pagination

--- SlickScheduler/SlickScheduler/Controllers/AdminController.cs
+++ SlickScheduler/SlickScheduler/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using SlickScheduler.Models;
+﻿using PagedList;
+using SlickScheduler.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +13,55 @@ namespace SlickScheduler.Controllers
         private DataModelContext db = new DataModelContext();
         // GET: Admin
         [HttpGet]
-        public ActionResult Index(String search)
+        public ActionResult Index(string sortOrder, string currentFilter, string search, int? page)
         {
+            //holds the current sort order for use across multiple pages
+            ViewBag.CurrentSort = sortOrder;
+            //Determines which sort to link to for name and Wnumber
+            ViewBag.NameSort = String.IsNullOrEmpty(sortOrder) ? "Name_Desc" : "";
+            ViewBag.WNumSort = sortOrder == "WNum" ? "WNum_Desc" : "WNum";
+            //Determins if this is the first page and handles filters
+            if (search != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                search = currentFilter;
+            }
+
+            //saves current filter for use in view
+            ViewBag.CurrentFilter = search;
+
+            //sets up a list of users
             var users = from u in db.Users
                         select u;
+            //gets whatever users match the current search
             if (!String.IsNullOrEmpty(search))
             {
                 users = users.Where(s => s.FirstName.Contains(search) || s.LastName.Contains(search) || s.WNumber.Contains(search));
             }
-            return View(users);
+
+            //sorts the data
+            switch (sortOrder)
+            {
+                case "Name_Desc":
+                    users = users.OrderByDescending(u => u.FirstName);
+                    break;
+                case "WNum":
+                    users = users.OrderBy(u => u.WNumber);
+                    break;
+                case "WNum_Desc":
+                    users = users.OrderByDescending(u => u.WNumber);
+                    break;
+                default:
+                    users = users.OrderBy(u => u.FirstName);
+                    break;
+            }
+
+            int pageSize = 25;
+            int pageNumber = (page ?? 1);
+            return View(users.ToPagedList(pageNumber, pageSize));
         }
         
     }
